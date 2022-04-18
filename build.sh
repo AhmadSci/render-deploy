832190406b25054119ba312fcb2a44ca8c03bff4
Ahmed Gamal <sci.ahmedgamal40520@alexu.edu.eg>
2022-04-18 02:29:51 +0200

    fix

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ poetry run pip install -r requirements.txt
 
 
 python manage.py collectstatic --no-input
-python manage.py migrate
 python manage.py makemigrations
+python manage.py migrate
 python manage.py createsuperuser
 ahmed
 ahmed@ahmed.com

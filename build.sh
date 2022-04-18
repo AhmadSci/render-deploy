0c2eff7c6920eee73ea7bf050a34b94130676243
Ahmed Gamal <sci.ahmedgamal40520@alexu.edu.eg>
2022-04-18 02:06:44 +0200

    bash

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 set -o errexit
 
 poetry install
+poetry run pip install dj_database_url
+poetry run pip install psycopg2
+poetry run pip install selenium
+poetry run pip install parsel
 
 python manage.py collectstatic --no-input
 python manage.py migrate
